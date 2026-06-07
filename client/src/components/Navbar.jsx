78392a072037f2025df97e280385454b538fc6b2
Patryk Rolka <89419456+rols0n@users.noreply.github.com>
2026-06-07 20:04:51 +0200

    Fixed some errors

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar__brand">
-        <span className="brand-icon">🎬</span>
+        
         <span>CINE</span>
         <strong>LIST</strong>
       </div>
@@ -10,7 +10,7 @@ export default function Navbar() {
       <div className="navbar__links">
         <a href="#home">Home</a>
         <a href="#movies">Filmy</a>
-        <a href="#add">+ Dodaj</a>
+       
       </div>
     </nav>
   );
